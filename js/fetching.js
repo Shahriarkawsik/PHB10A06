2c98a3b25d0c387d9741bb9877dba69fbeaebc9f
Shahriar <shahriarkawsik@gmail.com>
2024-10-07 21:25:12 +0600

    showing single category pets

--- js/fetching.js
+++ js/fetching.js
@@ -1,79 +1,73 @@
-// categoryBtn
-const loadCategoryBtn = ()=>{
-  fetch('https://openapi.programming-hero.com/api/peddy/categories')
-  .then(res => res.json())
-  .then(data => displayCategoryBtn(data.categories))
+// displaySingleCategoryPets
+// function displaySingleCategoryPets(pets){
+//   for (const pet of pets) {
+//     console.log(pet);
+    
+//   }
+// }
+// loadSingleCategoryPets
+async function loadSingleCategoryPets(id){
+  try{
+    const res =await fetch(`https://openapi.programming-hero.com/api/peddy/category/${id}`);
+    const {data} = await res.json();
+    displayAllPets(data);
+  }
+  catch(error){
+    console.error('Error fetching data:', error);
+  }
+    
+  // console.log(id);
 }
-loadCategoryBtn();
-// //displayCategoryBtn 
-const displayCategoryBtn = (categories) => {
+//displayCategoryBtn 
+function displayCategoryBtn(categories) {
   const categoryBtnSection = document.getElementById("categoryBtn");
   for (const item of categories) {
-    // const categoryBtnID = item.id;
-    // console.log(categoryBtnID);
-    const categoryBtn = document.createElement("div");
-    categoryBtnSection.appendChild(categoryBtn);
-    categoryBtn.classList.add('categoryBtn','inactiveBtn');
-    categoryBtn.innerHTML = `      
-      <img src="${item.category_icon}" alt="" />
-      <h1 class="font-bold text-24 leading-7">${item.category}</h1>
+    // console.log(item.category);
+    const categoryBtnContainer = document.createElement("div");
+    categoryBtnSection.appendChild(categoryBtnContainer);
+    categoryBtnContainer.classList.add('categoryBtn', 'inactiveBtn');
+    categoryBtnContainer.innerHTML = `
+      <button class="flex justify-center items-center gap-4" onclick="loadSingleCategoryPets('${item.category}')">
+        <img src="${item.category_icon}" alt="" />
+        <h1 class="font-bold text-24 leading-7">${item.category}</h1>
+      </button>   
     `;
   }
-};
-
-// Fetch All Pets
-const loadAllPets = () =>{
-  fetch('https://openapi.programming-hero.com/api/peddy/pets')
-  .then(res => res.json())
-  // .then(data => displayAllPetsTest(data.pets))
-  .then(data => displayAllPets(data.pets))
-  
 }
-loadAllPets()
-// display all pets
-const displayAllPetsTest = (pets) =>{
-  const petsContainer = document.getElementById("petsContainer");
-  for (const pet of pets) {
-    
-    const petCard = document.createElement("div");
-    petsContainer.appendChild(petCard);
-    petCard.classList.add('p-5', 'border', 'border-color1.1', 'rounded-xl', 'space-y-3');
-    petCard.innerHTML = `
-    <img
-      class="rounded-lg mb-6 w-full"
-      src="${pet.image}"
-      alt=""
-    />
-    <div class="space-y-2">
-      <h2 class="font-bold text-xl leading-6 text-color1">
-      ${pet.pet_name}
-      </h2>
-      <div class="space-y-2 text-color1.7 leading-5">
-        <div class="flex items-center gap-3">
-          <img src="./images/breed.svg" alt="" />
-          <p>Breed: ${(typeof pet.breed === "string") ? pet.breed : "Not available"}</p>
-        </div>
-        <div class="flex items-center gap-3">
-          <img src="./images/calender.svg" alt="" />
-          <p>Birth: ${(typeof pet.date_of_birth === "string") ? (pet.date_of_birth).slice(0,4) : "not Available"}</p>
-        </div>
-        <div class="flex items-center gap-3">
-          <img src="./images/gender.svg" alt="" />
-          <p>Gender: ${(typeof pet.gender === "string") ? pet.gender : "Not available"}</p>
-        </div>
-        <div class="flex items-center gap-3">
-          <img src="./images/price.svg" alt="" />
-          
-          <p>Price: ${(typeof pet.price === "number") ? pet.price + "$"  : "Not available"}</p>
-        </div>
-      </div>
-    </div>
-    `;
-  }
 
-};
-const displayAllPets = (pets) =>{
+// function displayCategoryBtn(categories){
+//   const categoryBtnSection = document.getElementById("categoryBtn");
+//   for (const item of categories) {
+//     // const btnId = item.category;
+//     console.log(item.category);
+//     const categoryBtnContainer = document.createElement("div");
+//     // const categoryBtn = document.createElement("button");
+//     categoryBtnSection.appendChild(categoryBtnContainer);
+//     categoryBtnContainer.classList.add('categoryBtn','inactiveBtn');
+//     categoryBtnContainer.innerHTML = `
+//       <button class = "flex justify-center items-center gap-4" onclick="loadSingleCategoryPets(${item.category})">
+//       <img src="${item.category_icon}" alt="" />
+//       <h1 class="font-bold text-24 leading-7">${item.category}</h1>
+//       </button>   
+//     `;
+//   }
+// };
+// fetchCategoryBtn
+async function loadCategoryBtn() {
+  try{
+    const res = await fetch('https://openapi.programming-hero.com/api/peddy/categories');
+    const data = await res.json();
+    displayCategoryBtn(data.categories);
+  }catch(error){
+    console.error('Error fetching data:', error);    
+  }
+}
+loadCategoryBtn();
+
+// display all pets
+function displayAllPets(pets){
   const petsContainer = document.getElementById("petsContainer");
+  petsContainer.innerHTML = "";
   for (const pet of pets) {
     // console.log(pet);
     const petCard = document.createElement("div");
@@ -129,3 +123,16 @@ const displayAllPets = (pets) =>{
     `;    
   }
 }
+// Fetch All Pets
+async function loadAllPets() {
+  try{
+    const res = await fetch('https://openapi.programming-hero.com/api/peddy/pets');
+    const data = await res.json();
+    displayAllPets(data.pets);
+  }
+  catch(error){
+    console.error('Error fetching data:', error);
+    
+  }
+}
+loadAllPets()
